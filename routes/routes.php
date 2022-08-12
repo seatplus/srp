a5decb54f4544ac42d62f9ae2de6e245dbb21de1
Felix Huber <felix.a.huber@gmx.net>
2022-08-12 14:32:05 +0200

    some refactoring

--- routes/routes.php
+++ routes/routes.php
@@ -30,32 +30,41 @@ use Seatplus\Srp\Http\Controller\AdminController;
 use Seatplus\Srp\Http\Controller\KillmailController;
 use Seatplus\Srp\Http\Controller\ReceiptController;
 use Seatplus\Srp\Http\Controller\RequestController;
+use Seatplus\Web\Http\Middleware\CheckACLPermission;
 use Seatplus\Web\Http\Middleware\CheckRequiredScopes;
 
 Route::middleware(['web', 'auth', CheckRequiredScopes::class])
     ->prefix('srp')
     ->group(function () {
-        Route::get('', [RequestController::class, 'index'])->name('srp.request');
-        Route::post('', [RequestController::class, 'store'])->name('store.srp.request');
 
-        Route::get('/view/{id}', [RequestController::class, 'viewRequest'])->name('view.srp.request');
-        Route::delete('/{request}/delete', [RequestController::class, 'delete'])->name('delete.srp.request');
-        Route::post('/submit/{id}', [RequestController::class, 'submitRequest'])->name('submit.srp.request');
-        Route::post('/review/{id}', [RequestController::class, 'handleRequest'])
-            ->middleware('permission:accept or reject srp requests')
-            ->name('handle.srp.request');
+        Route::controller(RequestController::class)
+            ->group(function () {
+                Route::get('', 'index')->name('srp.request');
+                Route::post('', 'store')->name('store.srp.request');
 
-        Route::get('/{location_id}/items', [KillmailController::class, 'items'])->name('get.killmail.items');
-        Route::post('/{item_id}/item', [KillmailController::class, 'updateItem'])->name('post.killmail.item');
+                Route::get('/view/{id}', 'viewRequest')->name('view.srp.request');
+                Route::delete('/{request}/delete', 'delete')->name('delete.srp.request');
+                Route::post('/submit/{id}', 'submitRequest')->name('submit.srp.request');
+                Route::post('/review/{id}', 'handleRequest')
+                    ->middleware('permission:accept or reject srp requests')
+                    ->name('handle.srp.request');
+            });
 
-        Route::middleware('permission:accept or reject srp requests')
+        Route::controller(KillmailController::class)
+            ->group(function () {
+                Route::get('/{location_id}/items', 'items')->name('get.killmail.items');
+                Route::post('/{item_id}/item', 'updateItem')->name('post.killmail.item');
+            });
+
+        Route::controller(AdminController::class)
+            ->middleware(CheckACLPermission::class . ':accept or reject srp requests')
             ->prefix('admin')
             ->group(function () {
-                Route::get('', [AdminController::class, 'review'])->name('review.srp.requests');
-                Route::get('/payout', [AdminController::class, 'payout'])->name('payout.srp.requests');
-                Route::post('/payout', [AdminController::class, 'processPayout'])->name('process.payout.srp.requests');
-                Route::get('/payout/data', [AdminController::class, 'payoutData'])->name('payout.data.srp.requests');
-                Route::get('/receipts', [AdminController::class, 'receipts'])->name('receipts.srp.requests');
+                Route::get('', 'review')->name('review.srp.requests');
+                Route::get('/payout', 'payout')->name('payout.srp.requests');
+                Route::post('/payout', 'processPayout')->name('process.payout.srp.requests');
+                Route::get('/payout/data', 'payoutData')->name('payout.data.srp.requests');
+                Route::get('/receipts', 'receipts')->name('receipts.srp.requests');
             });
 
         Route::get('receipt/{uuid}', [ReceiptController::class, 'index'])->name('view.srp.receipt');
